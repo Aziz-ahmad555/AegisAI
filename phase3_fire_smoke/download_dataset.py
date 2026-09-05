eb5065c0750c8c5474e66d3a32a49b72a5847a45
Aziz Ahmad <azizahmad5552@gmail.com>
2026-09-06 01:19:07 +0500

    Security fix: remove hardcoded API key, use environment variable

--- phase3_fire_smoke/download_dataset.py
+++ phase3_fire_smoke/download_dataset.py
@@ -1,6 +1,7 @@
-﻿from roboflow import Roboflow
+﻿import os
+from roboflow import Roboflow
 
-rf = Roboflow(api_key="56TpUtANhNkiN2fYclyG")
+rf = Roboflow(api_key=os.environ["ROBOFLOW_API_KEY"])
 project = rf.workspace("mahmoud-9wyf6").project("fire-n-smoke-detection")
 version = project.version(1)
 dataset = version.download("yolov8")
