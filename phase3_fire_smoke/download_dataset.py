87af2c2248582652cda9765a567dedd972835cda
Aziz Ahmad <azizahmad5552@gmail.com>
2026-09-06 01:15:05 +0500

    Restructure: move git repo root to top-level AegisAI folder

---
+++ phase3_fire_smoke/download_dataset.py
@@ -0,0 +1,8 @@
+﻿from roboflow import Roboflow
+
+rf = Roboflow(api_key="56TpUtANhNkiN2fYclyG")
+project = rf.workspace("mahmoud-9wyf6").project("fire-n-smoke-detection")
+version = project.version(1)
+dataset = version.download("yolov8")
+
+print("Dataset downloaded to:", dataset.location)
